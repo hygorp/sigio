9d7387b1ecf17a6745cae8a1fb0592c0d5f60036
Hygor <hygor.henrique@live.com>
2019-03-01 20:54:30 -0300

    upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sigio - Login</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/index.stylesheet.css">
+        <link rel="stylesheet" type="text/css" href="assets/semantic/semantic.min.css">
+        <script type="text/javascript" src="assets/semantic/semantic.min.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
