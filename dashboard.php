2a283b4ac040edb5a8211306fd1eeea2c23853eb
Hygor Henrique <hygor.henrique@live.com>
2019-03-15 18:21:01 -0300

    updates

--- dashboard.php
+++ dashboard.php
@@ -6,7 +6,6 @@
         <link rel="icon" href="assets/images/fav_sigio.png" type="image/png" sizes="16x16">
         <link rel="stylesheet" type="text/css" href="assets/css/dashboard.stylesheet.css">
         <link rel="stylesheet" type="text/css" href="assets/semantic/semantic.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/font-awesome/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="assets/toastr/toastr.min.css">
         <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="assets/semantic/semantic.min.js"></script>
@@ -20,6 +19,13 @@
         </div>
         
         <div class="sidenav">
+            <div class="sidenav-logo">
+                
+            </div>
+            <div class="sidenav-logo-responsive">
+                <img src="assets/images/sidenav-sigio.png" width="40">
+            </div>
+            
             <div class="menu-accordion">
                 <button class="accordion-button">
                     <i class="users icon"></i>
@@ -57,8 +63,22 @@
                 <!--menu-responsive-->
                 
                 <div class="menu-responsive">
-                    <div class="menu-item-responsive menu-item-responsive-first">
+                    <div class="menu-item-responsive">
                         <i class="big users icon"></i>
+                        <div class="dropdown-content">
+                            <a href="#">
+                                <i class="caret right icon"></i>
+                                Cadastro
+                            </a>
+                            <a href="#">
+                                <i class="caret right icon"></i>
+                                Link 2
+                            </a>
+                            <a href="#">
+                                <i class="caret right icon"></i>
+                                Link 3
+                            </a>
+                        </div>
                     </div>
                     
                     <div class="menu-item-responsive">
