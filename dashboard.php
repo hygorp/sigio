4f3e88a0c3353ab927ed900538f170555f703fe8
Hygor Henrique <hygor.henrique@live.com>
2019-03-15 02:50:44 -0300

    updates

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,7 @@
         <link rel="icon" href="assets/images/fav_sigio.png" type="image/png" sizes="16x16">
         <link rel="stylesheet" type="text/css" href="assets/css/dashboard.stylesheet.css">
         <link rel="stylesheet" type="text/css" href="assets/semantic/semantic.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/font-awesome/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="assets/toastr/toastr.min.css">
         <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="assets/semantic/semantic.min.js"></script>
@@ -14,6 +15,62 @@
     </head>
     
     <body>
+        <div class="sg-header">
+            
+        </div>
+        
+        <div class="sidenav">
+            <div class="menu-accordion">
+                <button class="accordion-button">
+                    <i class="users icon"></i>
+                    Pacientes
+                </button>
+                <div class="panel">
+                    <button class="accordion-item">
+                        <i class="caret right icon"></i>
+                        Hello
+                    </button>
+                </div>
+
+                <button class="accordion-button">
+                    <i class="user md icon"></i>
+                    Dentistas
+                </button>
+                <div class="panel">
+                    <button class="accordion-item">
+                        <i class="caret right icon"></i>
+                        Hello
+                    </button>
+                </div>
+
+                <button class="accordion-button">
+                    <i class="address book icon"></i>
+                    Agenda
+                </button>
+                <div class="panel">
+                    <button class="accordion-item">
+                        <i class="caret right icon"></i>
+                        Hello
+                    </button>
+                </div>
+                
+                <!--menu-responsive-->
+                
+                <div class="menu-responsive">
+                    <div class="menu-item-responsive menu-item-responsive-first">
+                        <i class="big users icon"></i>
+                    </div>
+                    
+                    <div class="menu-item-responsive">
+                        <i class="big user md icon"></i>
+                    </div>
+                    
+                    <div class="menu-item-responsive">
+                        <i class="big address book icon"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         <script type="text/javascript" src="assets/js/dashboard.accordion.menu.js"></script>
     </body>
